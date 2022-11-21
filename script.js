9c01957b03dc2548431c30d6add9fb9a64e55bcd
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2022-11-21 19:55:37 -0300

    Create date and shows it in html file

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let fullDate = new Date();
+let stringDate = fullDate.toDateString();
+let splitedDate = stringDate.split(" ");
+let today = ` ${splitedDate[2]} ${splitedDate[1]} ${splitedDate[3]}`;
+document.querySelector(".date").innerText = today;
